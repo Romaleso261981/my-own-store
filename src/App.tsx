a5b6cb14b7d5989b696b3a14e30451db66734fdc
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-14 13:29:41 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {HeaderSearch} from "./features/Header/Header.tsx";
+
+function App() {
+
+    return (
+        <>
+            <HeaderSearch/>
+        </>
+    )
+}
+
+export default App
