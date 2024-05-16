dc315c5a65e3f6a05bdf1aebba77df67ab5855ed
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-16 08:50:37 +0300

    some bag fix

--- src/widgets/ConditionerSelection/ConditionerSelection.tsx
+++ src/widgets/ConditionerSelection/ConditionerSelection.tsx
@@ -51,65 +51,68 @@ export default function ConditionerSelection() {
   return (
     <div className={s.wrapper}>
       <div className={s.container}>
-        <div className={s.block1}></div>
         <h2>
           Підберіть кондиціонери в 3 кліки та отримайте відповідні варіанти{" "}
           <br /> на email:
         </h2>
         <h4>Крок 1 із 3:</h4>
         <form action="submit">
-          <label htmlFor="">
-            {step === 1
-              ? "Висота стелі:"
-              : step === 2
-              ? "Кількість людей:"
-              : "Марка кондиціонера:"}
-          </label>
-          {step === 1 ? (
-            <input
-              type="text"
-              name="CeilingHeight"
-              onChange={(e) => handleChange(e)}
-            />
-          ) : step === 2 ? (
-            <input
-              type="text"
-              name="NumberOfPeople"
-              onChange={(e) => handleChange(e)}
-            />
-          ) : (
-            <input
-              type="text"
-              name="AirAonditionerBrand"
-              onChange={(e) => handleChange(e)}
-            />
-          )}
-          <label htmlFor="">
-            {step === 1
-              ? "Площа приміщення:"
-              : step === 2
-              ? "Кількість вікон"
-              : "Ваш номер телефону:"}
-          </label>
-          {step === 1 ? (
-            <input
-              type="text"
-              name="roomArea"
-              onChange={(e) => handleChange(e)}
-            />
-          ) : step === 2 ? (
-            <input
-              type="text"
-              name="NumberOfWindows"
-              onChange={(e) => handleChange(e)}
-            />
-          ) : (
-            <input
-              type="text"
-              name="PhoneNumber"
-              onChange={(e) => handleChange(e)}
-            />
-          )}
+          <div className={s.inputBlock}>
+            <label htmlFor="">
+              {step === 1
+                ? "Висота стелі:"
+                : step === 2
+                ? "Кількість людей:"
+                : "Марка кондиціонера:"}
+            </label>
+            {step === 1 ? (
+              <input
+                type="text"
+                name="CeilingHeight"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : step === 2 ? (
+              <input
+                type="text"
+                name="NumberOfPeople"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <input
+                type="text"
+                name="AirAonditionerBrand"
+                onChange={(e) => handleChange(e)}
+              />
+            )}
+          </div>
+          <div className={s.inputBlock}>
+            <label htmlFor="">
+              {step === 1
+                ? "Площа приміщення:"
+                : step === 2
+                ? "Кількість вікон"
+                : "Ваш номер телефону:"}
+            </label>
+            {step === 1 ? (
+              <input
+                type="text"
+                name="roomArea"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : step === 2 ? (
+              <input
+                type="text"
+                name="NumberOfWindows"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <input
+                type="text"
+                name="PhoneNumber"
+                onChange={(e) => handleChange(e)}
+              />
+            )}
+          </div>
           <button onClick={() => setStepChanger()} type="button">
             {step === 1
               ? "перейти до кроку №2"
